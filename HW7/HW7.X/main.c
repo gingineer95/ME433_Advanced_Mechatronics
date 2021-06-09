2e96c6640d48c8417831e58d95cd3dc95f2674fa
kailey.smith <kaileysmith2021@u.northwestern.edu>
2021-06-09 18:51:19 -0500

    Modified HW7 main.c to make the comments look nicer

--- HW7/HW7.X/main.c
+++ HW7/HW7.X/main.c
@@ -1,28 +1,28 @@
 #include <xc.h>           // processor SFR definitions
 #include <sys/attribs.h>  // __ISR macro
 #include <stdio.h>
-#include <math.h> //generate sine wave
+#include <math.h>         //generate sine wave
 #define PI 3.14159265
 
 // DEVCFG0
-#pragma config DEBUG = OFF // disable debugging
+#pragma config DEBUG = OFF  // disable debugging
 #pragma config JTAGEN = OFF // disable jtag
+#pragma config PWP = OFF    // disable flash write protect
+#pragma config BWP = OFF    // disable boot write protect
+#pragma config CP = OFF     // disable code protect
 #pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
-#pragma config PWP = OFF // disable flash write protect
-#pragma config BWP = OFF // disable boot write protect
-#pragma config CP = OFF // disable code protect
 
 // DEVCFG1
-#pragma config FNOSC = FRCPLL // use internal oscillator with pll
-#pragma config FSOSCEN = OFF // disable secondary oscillator
-#pragma config IESO = OFF // disable switching clocks
-#pragma config POSCMOD = OFF // RC mode
-#pragma config OSCIOFNC = OFF // disable clock output
-#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
-#pragma config FCKSM = CSDCMD // disable clock switch and FSCM
-#pragma config WDTPS = PS1048576 // use largest wdt
-#pragma config WINDIS = OFF // use non-window mode wdt
-#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FNOSC = FRCPLL       // use internal oscillator with pll
+#pragma config FSOSCEN = OFF        // disable secondary oscillator
+#pragma config IESO = OFF           // disable switching clocks
+#pragma config POSCMOD = OFF        // RC mode
+#pragma config OSCIOFNC = OFF       // disable clock output
+#pragma config FPBDIV = DIV_1       // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD       // disable clock switch and FSCM
+#pragma config WDTPS = PS1048576    // use largest wdt
+#pragma config WINDIS = OFF         // use non-window mode wdt
+#pragma config FWDTEN = OFF         // wdt disabled
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
@@ -31,9 +31,9 @@
 #pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
 
 // DEVCFG3
-#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config USERID = 0     // some 16bit userid, doesn't matter what
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
-#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF  // allow multiple reconfigurations
 
 unsigned short genSine(unsigned int i, unsigned short v);
 unsigned short genTriangle(unsigned int i, unsigned short v);
@@ -106,16 +106,16 @@ int main() {
             p1 = p1 | (vf1<<2);
 
             LATAbits.LATA0 = 0; //bring CS low
-            spi_io(p>>8); //write the byte
+            spi_io(p>>8);       //write the byte
             spi_io(p);
             LATAbits.LATA0 = 1; //bring CS high */
             
             LATAbits.LATA0 = 0; //bring CS low
-            spi_io(p1>>8); //write the byte
+            spi_io(p1>>8);      //write the byte
             spi_io(p1);
             LATAbits.LATA0 = 1; //bring CS high
             
-            _CP0_SET_COUNT(0); //delay here
+            _CP0_SET_COUNT(0);  //delay here
             while (_CP0_GET_COUNT() < 48000000 / (100)) { // 1kHz
                 ;
             }
@@ -129,7 +129,6 @@ int main() {
 unsigned short genSine(unsigned int i, unsigned short v) {
     double ret, val;
     val = PI/180;
-    // ret = sin(8*i*val); //2Hz frequency
     ret = 512.0*(sin((4*PI*(i/(100/2.0)))))+512.0;
 
     return(ret);
